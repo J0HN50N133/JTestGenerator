5c7757ceed4570e7957664370461dac94014375e
JohnsonLee <0xjohnsonlee@gmail.com>
2021-12-19 17:09:32 +0800

    feat: Z3StringApi Binding

---
+++ src/main/java/jtg/solver/Z3StringApi.java
@@ -0,0 +1,18 @@
+package jtg.solver;
+
+import com.microsoft.z3.*;
+
+public class Z3StringApi {
+    private Z3StringApi(){}
+    public static BoolExpr Length(Context ctx, String symbolName, int len){
+        StringSymbol symbol = ctx.mkSymbol(symbolName);
+        FuncDecl decl = ctx.mkConstDecl(symbol, ctx.mkStringSort());
+        return ctx.parseSMTLIB2String(
+                String.format("(assert (= (str.len %s) %d))", symbolName, len),
+                null,
+                null,
+                new Symbol[]{symbol},
+                new FuncDecl[]{decl}
+        )[0];
+    }
+}
